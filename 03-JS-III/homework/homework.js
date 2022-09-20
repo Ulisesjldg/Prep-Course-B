863b17f2cd0052e454956b4e333c8f4c8bc23433
ulisesjldg <ulisesjldg@gmail.com>
2022-09-19 21:20:38 -0500

    Hw03

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -3,34 +3,70 @@
 // Asegúrate que usas "return" cuando la consola te lo pida.
 // Pista: "console.log()" NO fucionará.
 // No cambies los nombres de las funciones.
-
+ 
 function imprimirSumaNumeros() {
-    // Imprime la suma de los números del 1 al 10
-    // Pista: usa un acumulador
-    // Tu código:
-}
+  // Devolver la suma de los números del 1 al 10
+  // Pista: usa un acumulador
+  // Tu código:
+  var acumulador = 0;
+  for (var i =1 ; i< 11 ; i++){
+    acumulador = acumulador + i    
+  }
+  return acumulador 
+ /*  //otra forma
+  var acumulador =0;
+  var i =1;
+  while (i<11){
+    acumulador = acumulador + i;
+    i++;
+  }
+  return acumulador */  
+};
+  
+
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var pares = [];
+  for (var i = 0; i<array.length ; i++){
+    if (array[i] %2 === 0){
+      pares.push(array[i])           
+    }    
+  }
+  return pares
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map(function(elementos){
+     return elementos ** 2
+  })
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  var acumulador = 0;
+  for (var i = 0 ; i<array.length; i++){
+  acumulador = acumulador + array[i];
+  }
+  return acumulador
+
+  /* // Otra forma
+  return array.reduce(function(acumulador, valor){
+  return acumulador + valor
+  }) */
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length
 }
   
   
